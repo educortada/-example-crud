3de612ba9eb46e79bc6f0d575d16a12a90142b15
Edu Cortada <cortadaedu@gmail.com>
2019-03-02 01:33:57 +0100

    Done

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/tortillApp', {
+  keepAlive: true,
+  useNewUrlParser: true,
+  reconnectTries: Number.MAX_VALUE
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,9 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-// -- 404 and error handler
-
-// NOTE: requires a views/not-found.ejs template
 app.use((req, res, next) => {
   res.status(404);
   res.render('not-found');
@@ -37,17 +41,6 @@ app.use((err, req, res, next) => {
     res.status(500);
     res.render('error');
   }
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
